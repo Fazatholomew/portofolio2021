f266265df6f7df6bcc40642f7ca595736a23905e
Bang Koboi <TheManHimself@jimmyganteng.com>
2022-01-24 03:11:14 -0500

    service worker request with failed path

--- serviceworker.js
+++ serviceworker.js
@@ -1,16 +1,17 @@
 var staticCacheName = "pwa";
- 
+
 self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+      return cache.addAll(["./index.html",
+        "./assets/images/background.jpg", "./assets/images/background1.jpg.jpg", "assets/images/background.jpg.jpg"]);
     })
   );
 });
- 
+
 self.addEventListener("fetch", function (event) {
   console.log(event.request.url);
- 
+
   event.respondWith(
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
